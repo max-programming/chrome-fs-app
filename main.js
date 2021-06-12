ad8603bb8d6ef8b71bef9bceba83271feebfd4bb
Max Programming <ui.sabuwala@gmail.com>
2021-06-12 06:38:29 +0530

    Removing other elements function

--- main.js
+++ main.js
@@ -11,7 +11,24 @@ const imgContentTag = document.getElementById("imgContent");
 const vidContentTag = document.getElementById("vidContent");
 const fileNameTag = document.getElementById("fileName");
 
-const hideVideoPlayer = () => {
+let isTxtOpen = false;
+let isImageOpen = false;
+let isVideoOpen = false;
+
+const removeOtherElements = () => {
+  if (!isImageOpen) {
+    imgContentTag.setAttribute("src", "");
+  }
+  if (!isVideoOpen) {
+    vidContentTag.style.display = "none";
+    vidContentTag.setAttribute("src", "");
+  }
+  if (!isTxtOpen) {
+    fileContentTag.innerText = "";
+  }
+};
+
+export const hideVideoPlayer = () => {
   if (vidContentTag.getAttribute("src") === "") {
     vidContentTag.style.display = "none";
   } else {
@@ -25,7 +42,12 @@ textSelect.onclick = async () => {
   const { contents, file } = await getTextFile();
   fileContentTag.innerText = contents;
   fileNameTag.innerText = `File name: ${file.name}`;
+  isTxtOpen = true;
+  isImageOpen = false;
+  isVideoOpen = false;
   hideVideoPlayer();
+  removeOtherElements();
+  console.log({ isTxtOpen, isImageOpen, isVideoOpen });
 };
 
 imgSelect.onclick = async () => {
@@ -33,7 +55,12 @@ imgSelect.onclick = async () => {
   const imgUrl = bufferToURL(arrayBuffer, file.type);
   imgContentTag.src = imgUrl;
   fileNameTag.innerText = `File name: ${file.name}`;
+  isImageOpen = true;
+  isTxtOpen = false;
+  isVideoOpen = false;
   hideVideoPlayer();
+  removeOtherElements();
+  console.log({ isTxtOpen, isImageOpen, isVideoOpen });
 };
 
 vidSelect.onclick = async () => {
@@ -41,5 +68,10 @@ vidSelect.onclick = async () => {
   const vidUrl = bufferToURL(arrayBuffer, file.type);
   vidContentTag.src = vidUrl;
   fileNameTag.innerText = `File name: ${file.name}`;
+  isVideoOpen = true;
+  isImageOpen = false;
+  isTxtOpen = false;
   hideVideoPlayer();
+  removeOtherElements();
+  console.log({ isTxtOpen, isImageOpen, isVideoOpen });
 };
